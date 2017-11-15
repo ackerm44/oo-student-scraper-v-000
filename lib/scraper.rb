13a037caf5d88ac27ba0dbdad6abb6c244f23012
Amanda Ackerman <ackerm44@gmail.com>
2017-11-15 18:27:02 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,31 +3,44 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
   def self.scrape_index_page(index_url)
-    index_url = File.read('fixtures/student-site/index.html')
-    doc = Nokogiri::HTML(index_url)
-
-    students_hash = []
-
-    doc.css("div.student-card").each do |student|
-      students_hash << {
-      :name => student.css("h4.student-name").text,
-      :location => student.css("p.student-location").text,
-      :profile_url => "./fixtures/student-site/" + student.css("a").attribute("href").value
-      }
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_profile_link = "#{student.attr('href')}"
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+      end
     end
-
-    students_hash
-
-    # student = doc.css(".student-card")
-    # student_name = doc.css("div.student-card").first.css("h4.student-name").text
-    # student_location = doc.css("div.student-card").first.css("p.student-location").text
-    # student_url = doc.css("div.student-card a").attribute("href").value
-
-    #binding.pry
-
+    students
   end
+
+  # def self.scrape_index_page(index_url)
+  #   index_url = File.read('fixtures/student-site/index.html')
+  #   doc = Nokogiri::HTML(index_url)
+  #
+  #   students_hash = []
+  #
+  #   doc.css("div.student-card").each do |student|
+  #     students_hash << {
+  #     :name => student.css("h4.student-name").text,
+  #     :location => student.css("p.student-location").text,
+  #     :profile_url => "./fixtures/student-site/" + student.css("a").attribute("href").value
+  #     }
+  #   end
+  #
+  #   students_hash
+  #
+  #   # student = doc.css(".student-card")
+  #   # student_name = doc.css("div.student-card").first.css("h4.student-name").text
+  #   # student_location = doc.css("div.student-card").first.css("p.student-location").text
+  #   # student_url = doc.css("div.student-card a").attribute("href").value
+  #
+  #   #binding.pry
+  #
+  # end
 
   def self.scrape_profile_page(profile_url)
 
