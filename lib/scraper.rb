5b2ff42cee5db222b88c781fae7189971b9eb7a7
Amanda Ackerman <ackerm44@gmail.com>
2017-11-15 20:45:13 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -42,7 +42,7 @@ class Scraper
    github_value = social.grep(/github/).join
    twitter_value = social.grep(/twitter/).join
    linkedin_value = social.grep(/linkedin/).join
-   if social.last != github_value && social.last != twitter_value && social.last != linkedinvalue
+   if social.last != github_value && social.last != twitter_value && social.last != linkedin_value
      blog_value = social.last
    end
    profile_quote_value = doc.css(".profile-quote").text
