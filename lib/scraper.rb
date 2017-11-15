102c2ee00dd398d34647a148bcee72a7d9d22457
Amanda Ackerman <ackerm44@gmail.com>
2017-11-15 18:32:17 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,33 +17,28 @@ class Scraper
     students
   end
 
-  # def self.scrape_index_page(index_url)
-  #   index_url = File.read('fixtures/student-site/index.html')
-  #   doc = Nokogiri::HTML(index_url)
-  #
-  #   students_hash = []
-  #
-  #   doc.css("div.student-card").each do |student|
-  #     students_hash << {
-  #     :name => student.css("h4.student-name").text,
-  #     :location => student.css("p.student-location").text,
-  #     :profile_url => "./fixtures/student-site/" + student.css("a").attribute("href").value
-  #     }
-  #   end
-  #
-  #   students_hash
-  #
-  #   # student = doc.css(".student-card")
-  #   # student_name = doc.css("div.student-card").first.css("h4.student-name").text
-  #   # student_location = doc.css("div.student-card").first.css("p.student-location").text
-  #   # student_url = doc.css("div.student-card a").attribute("href").value
-  #
-  #   #binding.pry
-  #
-  # end
+  def self.scrape_index_page(index_url)
+    index_url = File.read('fixtures/student-site/index.html')
+    doc = Nokogiri::HTML(index_url)
+  
+    students_hash = []
+
+  
+    doc.css("div.roster-cards-container").each do |card|
+      card.css("div.student-card").each do |student|
+        students_hash << {
+        :name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text,
+        :profile_url => "./fixtures/student-site/" + student.css("a").attribute("href").value
+        }
+      end
+    end
+  
+    students_hash
+
+  end
 
   def self.scrape_profile_page(profile_url)
-
   end
 
 end
