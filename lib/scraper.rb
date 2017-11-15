321867c53840b1de109d146d280d511c1d3a7d8a
Amanda Ackerman <ackerm44@gmail.com>
2017-11-15 20:04:10 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -53,4 +53,4 @@ class Scraper
 
 end
 
-Scraper.scrape_profile_page('/fixtures/student-site/students/jason-southwell.html')
+#Scraper.scrape_profile_page('/fixtures/student-site/students/jason-southwell.html')
