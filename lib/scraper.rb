d7d27870cbc84681fcacb5751054c7a2e76058bc
Amanda Ackerman <ackerm44@gmail.com>
2017-11-15 20:14:21 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -39,7 +39,7 @@ class Scraper
     # binding.pry
     #
      social = doc.css(".social-icon-container a").collect {|link| link['href']},
-     github_value = social.grep(/github/)
+     :github_value => social.grep(/github/)
     # twitter_value = social.grep(/twitter/)
     # linkedin_value = social.grep(/linkedin/)
     # blog_value = social.grep(/blog/)
