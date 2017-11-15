90ba6ab5f98f51ce5aee5dcce5895c9be2cd6750
Amanda Ackerman <ackerm44@gmail.com>
2017-11-15 18:37:30 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,7 +28,7 @@ class Scraper
         students_hash << {
         :name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
-        :profile_url => "./fixtures/student-site/" + student.css("a").attribute("href").value
+        :profile_url => "./fixtures/student-site/" + student.css("a").attribute("href")
         }
       end
     end
