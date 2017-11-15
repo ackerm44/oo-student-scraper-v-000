bc9f77fdb176bb15b337ea8d5868e24ef126b140
Amanda Ackerman <ackerm44@gmail.com>
2017-11-15 20:44:09 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -66,4 +66,4 @@ class Scraper
 
 end
 
-#Scraper.scrape_profile_page('fixtures/student-site/students/morgan-vanyperen.html')
+Scraper.scrape_profile_page('fixtures/student-site/students/morgan-vanyperen.html')
