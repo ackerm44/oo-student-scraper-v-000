4d30a50cdb0084d9030ea49fa8bf207b87697d12
Amanda Ackerman <ackerm44@gmail.com>
2017-11-15 18:06:06 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,39 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_url = File.read('fixtures/student-site/index.html')
+    doc = Nokogiri::HTML(index_url)
+
+    students_hash = {}
+
+    doc.css(".roster-cards-container").each do |student|
+      students_hash = {
+      :name => doc.css("div.student-card").first.css("h4.student-name").text,
+      :location => doc.css("div.student-card").first.css("p.student-location").text,
+      :profile_url => "./fixtures/student-site/" + doc.css("div.student-card a").attribute("href").value
+      }
+      puts students_hash
+    end
+
+    students_hash
+
+    # student = doc.css(".student-card")
+    # student_name = doc.css("div.student-card").first.css("h4.student-name").text
+    # student_location = doc.css("div.student-card").first.css("p.student-location").text
+    # student_url = doc.css("div.student-card a").attribute("href").value
+
+    #binding.pry
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+Scraper.scrape_index_page('../fixtures/student-site/index.html')
